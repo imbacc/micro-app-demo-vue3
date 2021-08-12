be526bd24b2851380f8984b7beff5992ab9e0de8
yueyiqianduan <9306797+yueyiqianduan@user.noreply.gitee.com>
2021-08-12 11:39:31 +0800

    frist

---
+++ src/common/tools/cmake_zintercept.js
@@ -0,0 +1,124 @@
+/**
+ * 拦截请求
+ */
+import axios from 'axios'
+import { env } from '@common/config/cfg.js'
+
+const BASE_API = env.VITE_GLOB_API_URL
+const http = axios.create({
+	baseURL: BASE_API, // url = base url + request url
+	timeout: 9000
+})
+
+const error_msg = (msg = '网络异常') => {
+	// element notice
+	console.log(msg)
+}
+const record = {}
+
+// 请求拦截器
+http.interceptors.request.use(
+	(config) => {
+		// 在发送请求之前做些什么
+
+		if (config.data && config.data._rollback) {
+			if (record[config.url]) return Promise.reject(new Error(`_roolback_${config.url}`))
+			record[config.url] = true
+		}
+
+		if (config.data._noToken) {
+			delete config.data['_noToken']
+			delete config.header['x-access-token']
+		}
+		
+		if (config.data._formData) {
+			config.header['content-Type'] = 'application/x-www-form-urlencoded'
+			delete config.data['_formData']
+		}
+		
+		if (config.data._header) {
+			config.header = { ...config.header, ...config.data._header }
+			delete config.data['_header']
+		}
+
+		// console.log("【config】 " + JSON.stringify(config))
+
+		return config
+	},
+	(error) => {
+		// 对请求错误做些什么
+		return Promise.reject(error)
+	}
+)
+
+// 响应拦截器
+http.interceptors.response.use(
+	(response) => {
+		// console.log("【response】 " + response)
+
+		if (response.status === 401) {
+			// removeToken
+			localStorage.clear()
+			window.location = '/login'
+			return
+		}
+		const res = response.data
+		if (res.status === 401) {
+			error_msg('401 error')
+			return Boolean(false)
+		}
+
+		if (res.status === 503) {
+			error_msg('503 error')
+			return Boolean(false)
+		}
+
+		if (res.data && res.data.error) {
+			error_msg('请求失败', res.data.error)
+			return Boolean(false)
+		}
+
+		return res
+	},
+	(error) => {
+		// 对响应错误做点什么
+		const err = error.toString()
+
+		// 回滚中断请求
+		if (err.indexOf('_roolback_') !== -1) {
+			const key = err.replace('_roolback_', '')
+			delete record[key]
+			return Boolean(false)
+		}
+
+		if (err.indexOf('code 500') !== -1) {
+			error_msg('500 error')
+			return Boolean(false)
+		}
+
+		if (err.indexOf('code 503') !== -1) {
+			error_msg('503 error')
+			return Boolean(false)
+		}
+
+		if (err.indexOf('code 401') !== -1) {
+			error_msg('401 error')
+			return Boolean(false)
+		}
+
+		if (err.indexOf('code 422') !== -1) {
+			// 服务器错误信息回应
+			error_msg(error.response && error.response.data && error.response.data.error, '')
+			return Boolean(false)
+		}
+
+		if (error.toString().indexOf('Error') !== -1) {
+			error_msg('网络异常')
+			return Boolean(false)
+		}
+
+		return Promise.reject(error)
+	}
+)
+
+export default http
